64324ff4625a62576990e5df4f0ae99fb6811b9d
Dhairya Vaghela <dhairyavaghela12@gmail.com>
2026-01-06 17:40:16 +0530

    Task-1 Completed

---
+++ Day-wise/Day-1/debug.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+for i in range(10):
+    print(f"{i+1} Debugging")
+
+print("No error found")
---
+++ Day-wise/Day-1/functions.py
@@ -0,0 +1,5 @@
+def add(no1,no2):
+    return no1+no2
+
+def area(radius,pie=3.14):
+    return 2 * pie * radius 
